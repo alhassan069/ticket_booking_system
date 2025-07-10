693befd416e4b2af3991d66ffd09b18a58ca61b6
Alimul Hassan <alhassan069@gmail.com>
2025-07-10 18:22:13 +0530

    Changed the import errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .database import Base, get_db
 from .models import Venue, Event, TicketType, Booking, BookingStatusEnum
 
+__all__ = ["Base", "get_db", "Venue", "Event", "TicketType", "Booking", "BookingStatusEnum"]
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,6 @@
-from . import router, venues, events, tickets, bookings
+from .venues import router as venues_router
+from .events import router as events_router
+from .tickets import router as tickets_router
+from .bookings import router as bookings_router
+
+__all__ = ["venues_router", "events_router", "tickets_router", "bookings_router"]
